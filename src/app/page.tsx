c3fe2162a3c6a5ac5186c11c7ff5396484e56c43
Chanuk Kim <cksdnr987@gmail.com>
2024-08-28 18:29:55 +0900

    add eslint-plugin-tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl flex flex-col items-center text-center lg:flex">
-        <Image src="/logo.png" alt="Logo" width="300" height="300" />
-        <h1 className="">YoursAI</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="z-10 w-full max-w-5xl lg:flex">
+        <article className="prose p-10 text-center lg:prose-xl">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width="300"
+            height="300"
+            className="mx-auto"
+          />
+          <h1>YoursAI</h1>
+          <p>Simple yet fully customizable character role-playing frontend.</p>
+        </article>
       </div>
     </main>
   );
