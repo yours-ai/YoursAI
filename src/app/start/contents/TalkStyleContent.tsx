74f311add575353835dd200b903392f8d74ffa7c
Seokryu Park <seokryu.park@pygmalion.team>
2024-09-04 13:00:16 +0900

    feature: floatingUI tooltip 적용

--- src/app/start/contents/TalkStyleContent.tsx
+++ src/app/start/contents/TalkStyleContent.tsx
@@ -1,12 +1,37 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import SettingTitle from "@/components/SettingTitle";
 import SegmentedControlBar from "@/components/SegmentedControlBar";
 import SegmentBoard from "@/components/SegmentBoard";
 import SetupControlButton from "@/components/SetupControlButton";
 import { PiQuestionBold } from "react-icons/pi";
-import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
+import {
+  arrow,
+  autoUpdate,
+  flip,
+  offset,
+  shift,
+  useDismiss,
+  useFloating,
+  useHover,
+  useInteractions,
+  useRole,
+} from "@floating-ui/react";
+import SetupForm from "@/components/SetupForm";
+
+const CustomStyleFormRows = [
+  {
+    rowTitle: "대화 스타일 파일",
+    rowDescription: "Risu 스타일의 프롬프트 템플릿도 지원합니다.",
+    action: <SetupControlButton custom="업로드" />,
+  },
+  {
+    rowTitle: "(선택) 모듈 파일",
+    rowDescription: "Risu 스타일의 모듈 파일도 지원합니다.",
+    action: <SetupControlButton custom="업로드" />,
+  },
+];
 
 const styles = [
   {
@@ -45,12 +70,83 @@ const styles = [
   {
     title: "커스텀",
     content: (
-      <div className="flex h-[223px] w-[421px] items-center justify-center">
-        <SetupControlButton custom="커스텀 테마 파일 업로드 하기" />
+      <div className="mt-[14px]">
+        <SetupForm setupFormRows={CustomStyleFormRows} />
       </div>
     ),
   },
 ];
+
+const Tooltip = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const arrowRef = useRef(null);
+  const { refs, floatingStyles, context, strategy, x, y, middlewareData } =
+    useFloating({
+      open: isOpen,
+      onOpenChange: setIsOpen,
+      middleware: [
+        offset(15),
+        flip(),
+        shift(),
+        arrow({ element: arrowRef, padding: 10 }),
+      ],
+      whileElementsMounted: autoUpdate,
+      placement: "top-start",
+    });
+  const hover = useHover(context, { delay: { open: 200, close: 0 } });
+  const dismiss = useDismiss(context);
+  const role = useRole(context, {
+    role: "tooltip",
+  });
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    hover,
+    dismiss,
+    role,
+  ]);
+
+  return (
+    <>
+      <div ref={refs.setReference} {...getReferenceProps()}>
+        <PiQuestionBold className="text-black/50" />
+      </div>
+      {isOpen && (
+        <div
+          ref={refs.setFloating}
+          className={`rounded-[10px] bg-white px-[10px] py-[9px] text-13p leading-[16px]`}
+          style={{
+            width: "max-content",
+            position: strategy,
+            top: y ?? 0,
+            left: x ?? 0,
+            zIndex: 10,
+            boxShadow:
+              "0px 8px 40px 0px rgba(0, 0, 0, 0.25), 0px 0px 3px 0px rgba(0, 0, 0, 0.55), 0px 0px 3px 0px rgba(255, 255, 255, 0.10)",
+          }}
+          {...getFloatingProps()}
+        >
+          RisuAi에 익숙하다면,
+          <br />
+          기존의 프롬프트 템플릿과 같습니다.
+          <div
+            ref={arrowRef}
+            style={{
+              position: "absolute",
+              width: "12px",
+              height: "12px",
+              background: "white",
+              transform: "rotate(45deg)",
+              top: "84%",
+              zIndex: 2,
+              left: middlewareData.arrow?.x ?? 0,
+              marginLeft: "1px",
+              borderRadius: "2px",
+            }}
+          />
+        </div>
+      )}
+    </>
+  );
+};
 
 function TalkStyleContent() {
   const [index, setIndex] = useState<number>(0);
@@ -58,14 +154,9 @@ function TalkStyleContent() {
     <>
       <div className="relative z-40 flex items-center">
         <SettingTitle title="대화 스타일 고르기" />
-        <Popover>
-          <PopoverButton>
-            <PiQuestionBold className="absolute bottom-[8px] right-[-24px] text-[15px] text-black/50" />
-          </PopoverButton>
-          <PopoverPanel anchor="bottom start" className="relative z-40">
-            <p>기존의 프롬프트 템플릿과 같습니다</p>
-          </PopoverPanel>
-        </Popover>
+        <div className="absolute bottom-[8px] right-[-30px]">
+          <Tooltip />
+        </div>
       </div>
 
       <div className="mt-[24px] flex flex-col items-center gap-[17px]">
