c8d479d150aa3cfb443084d0ca5f2468c001da6c
Seokryu Park <seokryu.park@pygmalion.team>
2024-09-06 18:11:41 +0900

    feature: 유저의 브라우저 언어에 따라 locale route 접속

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,8 +2,8 @@ import { defineRouting } from "next-intl/routing";
 import { createSharedPathnamesNavigation } from "next-intl/navigation";
 
 export const routing = defineRouting({
-  locales: ["kr", "en"],
-  defaultLocale: "kr",
+  locales: ["en", "kr"],
+  defaultLocale: "en",
 });
 
 export const { Link, redirect, usePathname, useRouter } =
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,25 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
+import { NextRequest, NextResponse } from "next/server";
 
-export default createMiddleware(routing);
+export default function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+
+  if (pathname.startsWith("/en") || pathname.startsWith("/kr")) {
+    return createMiddleware(routing)(req);
+  }
+
+  const acceptLanguage = req.headers.get("accept-language");
+  console.log(acceptLanguage);
+  const preferredLocale = acceptLanguage?.split(",")[0].split("-")[0];
+  console.log("preferredLocale:", preferredLocale);
+
+  const defaultLocale = preferredLocale === "ko" ? "kr" : "en";
+
+  return NextResponse.redirect(
+    new URL(`/${defaultLocale}${pathname}`, req.url),
+  );
+}
 
 export const config = {
   matcher: ["/", "/(kr|en)/:path*"],
