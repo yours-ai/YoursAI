834fa73bbcf0692a97541c4cd2abe0407fb1acba
Arsture <minseok.kim@pygmalion.team>
2024-09-06 16:31:38 +0900

    fix: deploy error

--- src/themes/chocolate/index.ts
+++ src/themes/chocolate/index.ts
@@ -1,11 +1,17 @@
 import Button from "./Button";
 import { Theme } from "@/themes/models";
+import FriendList from "@/themes/theFruit/FriendList";
+import FriendDetail from "@/themes/theFruit/FriendDetail";
+import EmptySpace from "@/themes/theFruit/EmptySpace";
 
 const chocolateTheme: Theme = {
   name: "초콜릿 테마",
   description: "한국의 가장 대표적인 메신저",
   components: {
     Button,
+    FriendList, //  TODO: chocolate 테마의 FriendList 컴포넌트를 추가해야함.
+    FriendDetail, //  TODO: chocolate 테마의 FriendList 컴포넌트를 추가해야함.
+    EmptySpace, //  TODO: chocolate 테마의 FriendList 컴포넌트를 추가해야함.
   },
 };
 
