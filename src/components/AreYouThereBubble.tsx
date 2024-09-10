4e87470b5a210925dbbfe9aa2f766b86fa8816f9
Seokryu Park <seokryu.park@pygmalion.team>
2024-09-10 16:36:17 +0900

    feature: 셋업 첫페이지 tooltip

--- src/components/AreYouThereBubble.tsx
+++ src/components/AreYouThereBubble.tsx
@@ -1,48 +1,157 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { TypeAnimation } from "react-type-animation";
 import { useTranslation } from "react-i18next";
+import {
+  arrow,
+  autoUpdate,
+  flip,
+  offset,
+  shift,
+  useDismiss,
+  useFloating,
+  useHover,
+  useInteractions,
+  useRole,
+} from "@floating-ui/react";
 
-function AreYouThereBubble() {
+function AreYouThereBubble({ children }: { children: React.ReactNode }) {
   const { t } = useTranslation("setup/areYouThere");
   const [isPrompt, setIsPrompt] = useState<boolean>(false);
   const [animationComplete, setAnimationComplete] = useState<boolean>(false);
+  const arrowRef = useRef(null);
+  const { refs, context, strategy, x, y } = useFloating({
+    open: true,
+    middleware: [
+      offset(15),
+      flip(),
+      shift(),
+      arrow({ element: arrowRef, padding: 10 }),
+    ],
+    whileElementsMounted: autoUpdate,
+    placement: "bottom-start",
+  });
+  const hover = useHover(context, { delay: { open: 0, close: 0 } });
+  const dismiss = useDismiss(context);
+  const role = useRole(context, {
+    role: "tooltip",
+  });
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    hover,
+    dismiss,
+    role,
+  ]);
+
   return (
-    <div className="relative rounded-[10px] bg-[#E9E9E9]/70 px-[17px] py-[9px] text-20p">
-      <div className={`${isPrompt ? "italic text-blue-800" : ""}`}>
-        {!animationComplete ? (
-          <TypeAnimation
-            sequence={[
-              t("systemPrompt"),
-              1000,
-              () => {
-                setIsPrompt(true);
-                setAnimationComplete(true);
-              },
-            ]}
-            wrapper="span"
-            cursor={false}
-          />
-        ) : (
-          <span>{t("systemPromptItalics")}</span>
-        )}
+    <>
+      <div ref={refs.setReference} {...getReferenceProps()}>
+        {children}
       </div>
-      {isPrompt ? (
-        <TypeAnimation
-          sequence={[
-            // "",
-            // 2300,
-            t("conversation"),
-            1000,
-            () => {
-              console.log("sequence completed");
-            },
-          ]}
-          wrapper="span"
-          cursor={false}
+      <div
+        ref={refs.setFloating}
+        className={`rounded-[10px] bg-white px-[17px] py-[10px] text-25p leading-[30px]`}
+        style={{
+          width: "max-content",
+          position: strategy,
+          top: y ?? 0,
+          left: x - 70,
+          zIndex: 10,
+          boxShadow:
+            "0px 8px 40px 0px rgba(0, 0, 0, 0.25), 0px 0px 3px 0px rgba(0, 0, 0, 0.55), 0px 0px 3px 0px rgba(255, 255, 255, 0.10)",
+        }}
+        {...getFloatingProps()}
+      >
+        <p className="relative z-20">
+          <div className={`${isPrompt ? "italic text-[#CA5858]" : ""}`}>
+            {!animationComplete ? (
+              <TypeAnimation
+                sequence={[
+                  t("systemPrompt"),
+                  1000,
+                  () => {
+                    setIsPrompt(true);
+                    setAnimationComplete(true);
+                  },
+                ]}
+                wrapper="span"
+                cursor={false}
+              />
+            ) : (
+              <span>{t("systemPromptItalics")}</span>
+            )}
+          </div>
+          {isPrompt ? (
+            <TypeAnimation
+              sequence={[
+                t("conversation"),
+                1000,
+                () => {
+                  console.log("sequence completed");
+                },
+              ]}
+              wrapper="span"
+              cursor={false}
+            />
+          ) : null}
+        </p>
+
+        <div
+          ref={arrowRef}
+          style={{
+            position: "absolute",
+            width: "12px",
+            height: "12px",
+            background: "white",
+            transform: "rotate(45deg)",
+            top: "-5px",
+            left: "10px",
+            zIndex: 2,
+            marginLeft: "1px",
+            borderRadius: "2px",
+            boxShadow:
+              "0px 8px 40px 0px rgba(0, 0, 0, 0.25), 0px 0px 3px 0px rgba(0, 0, 0, 0.55), 0px 0px 3px 0px rgba(255, 255, 255, 0.10)",
+          }}
         />
-      ) : null}
-    </div>
+        <div className="absolute inset-0 z-10 rounded-[10px] bg-white" />
+      </div>
+    </>
   );
+
+  //   <div className="relative rounded-[10px] bg-[#E9E9E9]/70 px-[17px] py-[9px] text-20p">
+  //     <div className={`${isPrompt ? "italic text-blue-800" : ""}`}>
+  //       {!animationComplete ? (
+  //         <TypeAnimation
+  //           sequence={[
+  //             t("systemPrompt"),
+  //             1000,
+  //             () => {
+  //               setIsPrompt(true);
+  //               setAnimationComplete(true);
+  //             },
+  //           ]}
+  //           wrapper="span"
+  //           cursor={false}
+  //         />
+  //       ) : (
+  //         <span>{t("systemPromptItalics")}</span>
+  //       )}
+  //     </div>
+  //     {isPrompt ? (
+  //       <TypeAnimation
+  //         sequence={[
+  //           // "",
+  //           // 2300,
+  //           t("conversation"),
+  //           1000,
+  //           () => {
+  //             console.log("sequence completed");
+  //           },
+  //         ]}
+  //         wrapper="span"
+  //         cursor={false}
+  //       />
+  //     ) : null}
+  //   </div>
+  // );
 }
 
 export default AreYouThereBubble;
--- src/routes/setup/SetupStart.tsx
+++ src/routes/setup/SetupStart.tsx
@@ -13,20 +13,21 @@ interface Props {
 function SetupStart({ setStep }: Props) {
   const { t } = useTranslation("setup/setupPage");
   return (
-    <div className="flex flex-col gap-[175px]">
+    <div className="flex flex-col gap-[200px]">
       <div className="flex flex-col items-center">
         <img src="/logo.png" alt="heart-logo" width={180} height={180} />
-        <img
-          src="/logo-letters.png"
-          alt="yours-ai"
-          width={192}
-          height={70}
-          className="relative bottom-5"
-        />
-        <AreYouThereBubble />
+        <AreYouThereBubble>
+          <img
+            src="/logo-letters.png"
+            alt="yours-ai"
+            width={192}
+            height={70}
+            className="relative bottom-5"
+          />
+        </AreYouThereBubble>
       </div>
       <div
-        className="start-button flex cursor-pointer flex-col items-center gap-[12px] text-[35px] font-bold text-white/50"
+        className="start-button relative top-[100px] flex cursor-pointer flex-col items-center gap-[12px] text-[35px] font-bold text-white/50"
         onClick={() => setStep((prev) => prev + 1)}
       >
         <div className="flex size-[67px] items-center justify-center rounded-full border-4 border-white/50 duration-200">
