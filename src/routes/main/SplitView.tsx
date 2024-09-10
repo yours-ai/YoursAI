a30e0b102c88871794cb336d163bf6b9024a6d4d
Chanuk Kim <cksdnr987@gmail.com>
2024-09-10 12:59:21 +0900

    feature: add 100vh

--- src/routes/main/SplitView.tsx
+++ src/routes/main/SplitView.tsx
@@ -1,3 +1,5 @@
+import { use100vh } from "react-div-100vh";
+
 export interface SplitViewProps {
   primary: "left" | "right";
   leftPane?: React.ReactNode;
@@ -13,10 +15,13 @@ export default function SplitView({
   rightPane,
   primary,
 }: SplitViewProps) {
+  const height = use100vh();
+
   return (
     <div className="relative flex">
       <div
-        className={`sticky top-0 h-screen tablet:basis-2/6 desktop:basis-1/4 ${primary === "left" ? "grow tablet:grow-0" : "hidden tablet:block"}`}
+        className={`sticky top-0 tablet:basis-2/6 desktop:basis-1/4 ${primary === "left" ? "grow tablet:grow-0" : "hidden tablet:block"}`}
+        style={{ height: height ?? undefined }}
       >
         <div className="relative size-full">
           <div
