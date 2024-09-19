73ce0ae2e0bdf8c3bfa95c2ad9674a50d421c1d6
Chanuk Kim <cksdnr987@gmail.com>
2024-09-19 19:01:48 +0900

    pass test

--- src/domain/utils/testDb.ts
+++ src/domain/utils/testDb.ts
@@ -1,12 +1,23 @@
 import "fake-indexeddb/auto";
 import { afterEach, beforeEach } from "vitest";
 import { Db, getDb } from "@/domain/db.ts";
+import { GlobalConfigId } from "@/domain/config/models.ts";
 
 export const getTestDb = (): { getDbInstance: () => Db } => {
   let db: Db;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     db = getDb();
+    const globalConfig = await db.globalConfigs.get(GlobalConfigId);
+    await db.globalConfigs.update(GlobalConfigId, {
+      conversationConfig: {
+        ...globalConfig!.conversationConfig,
+        persona: {
+          ...globalConfig!.conversationConfig.persona,
+          firstName: "찬욱",
+        },
+      },
+    });
   });
 
   afterEach(async () => {
