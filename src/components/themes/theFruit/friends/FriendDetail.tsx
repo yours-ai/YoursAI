8081f7422325fb64f989d90f4c2bf10de12623c3
Arsture <minseok.kim@pygmalion.team>
2024-10-04 18:13:12 +0900

    feature: move mock file

--- src/components/themes/theFruit/friends/FriendDetail.tsx
+++ src/components/themes/theFruit/friends/FriendDetail.tsx
@@ -21,10 +21,10 @@ const FriendsButton = ({
 }) => {
   return (
     <div
-      className={`phone:w-[132px] flex w-[100px] flex-col items-center justify-center gap-[6px] rounded-[8px] bg-black/40 py-[10px] ${disabled ? "text-white/25" : "cursor-pointer text-white hover:bg-black/50"} text-white backdrop-blur-sm duration-150`}
+      className={`flex w-[100px] flex-col items-center justify-center gap-[6px] rounded-[8px] bg-black/40 py-[10px] phone:w-[132px] ${disabled ? "text-white/25" : "cursor-pointer text-white hover:bg-black/50"} text-white backdrop-blur-sm duration-150`}
     >
-      <div className="phone:text-34p text-[30px]">{icon}</div>
-      <span className="text-12p phone:text-14p leading-[18px]">{label}</span>
+      <div className="text-[30px] phone:text-34p">{icon}</div>
+      <span className="text-12p leading-[18px] phone:text-14p">{label}</span>
     </div>
   );
 };
@@ -37,7 +37,7 @@ const ContactItem = ({
   content: string;
 }) => {
   return (
-    <div className="border-border phone:w-[417px]  w-full border-b p-[10px]">
+    <div className="w-full border-b  border-border p-[10px] phone:w-[417px]">
       <div className="text-14p">{label}</div>
       <div className="text-16p text-accentBlue">{content}</div>
     </div>
@@ -49,7 +49,7 @@ export default function FriendDetail({ friendId }: FriendDetailProps) {
   const [deleteDialogOpened, setDeleteDialogOpened] = useState<boolean>(false);
   return (
     <div className="size-full">
-      <div className="bg-tabUnselected relative flex items-center justify-center pb-[30px] pt-[50px]">
+      <div className="relative flex items-center justify-center bg-tabUnselected pb-[30px] pt-[50px]">
         <div className="absolute inset-0 z-0 overflow-hidden">
           <img
             src="/sena-background.jpg"
@@ -59,13 +59,13 @@ export default function FriendDetail({ friendId }: FriendDetailProps) {
           <div className="absolute inset-0 bg-black/10" />
         </div>
         <Link to="../">
-          <div className="text-24p text-accentBlue absolute left-[10px] top-[10px]">
+          <div className="absolute left-[10px] top-[10px] text-24p text-accentBlue">
             <PiCaretLeftBold />
           </div>
         </Link>
         <div className="relative z-10 flex flex-col items-center gap-[18px]">
           <img
-            src="/sena.png"
+            src="/mock/sena.jpg"
             alt="sena-profile-image"
             className="size-[120px] rounded-full object-cover object-center"
             style={{
@@ -97,13 +97,13 @@ export default function FriendDetail({ friendId }: FriendDetailProps) {
           </div>
         </div>
       </div>
-      <div className="phone:px-0 mt-[22px] flex flex-col items-center px-[10px]">
+      <div className="mt-[22px] flex flex-col items-center px-[10px] phone:px-0">
         <ContactItem label={t("contactItem.creator")} content="narayo9" />
         <ContactItem
           label={t("contactItem.creatorIntro")}
           content="조용하고 따듯한 마음을 가진, 독서를 즐기는 소녀입니다."
         />
-        <div className="text-14p text-red hover:text-redHover phone:w-[417px] w-full cursor-pointer p-[10px] duration-150">
+        <div className="w-full cursor-pointer p-[10px] text-14p text-red duration-150 hover:text-redHover phone:w-[417px]">
           <span onClick={() => setDeleteDialogOpened(true)}>
             {t("delete.label")}
           </span>
@@ -133,7 +133,7 @@ export default function FriendDetail({ friendId }: FriendDetailProps) {
             </DialogButton>
             <DialogButton
               onClick={() => setDeleteDialogOpened(false)}
-              className="text-red font-semibold"
+              className="font-semibold text-red"
             >
               {t("delete.dialog.yes")}
             </DialogButton>
--- src/components/themes/theFruit/messages/ChatRoom.tsx
+++ src/components/themes/theFruit/messages/ChatRoom.tsx
@@ -6,20 +6,20 @@ export default function ChatRoom({ chatRoomId }: ChatRoomProps) {
   console.log(chatRoomId); // TODO <- 삭제
   return (
     <div className="size-full">
-      <div className="phone:h-[98px] flex h-[59px] w-full items-center justify-between border-b-[0.5px] border-black/30 bg-black/5 pl-[10px] pr-[20px] backdrop-blur-[50px]">
+      <div className="flex h-[59px] w-full items-center justify-between border-b-[0.5px] border-black/30 bg-black/5 pl-[10px] pr-[20px] backdrop-blur-[50px] phone:h-[98px]">
         <Link to="../">
           <div className="text-24p text-accentBlue">
             <PiCaretLeftBold />
           </div>
         </Link>
-        <div className="phone:flex-col flex items-center gap-[8px]">
+        <div className="flex items-center gap-[8px] phone:flex-col">
           <img
-            src="/sena.png"
+            src="/mock/sena.jpg"
             alt="sena-image"
-            className="phone:size-[40px] size-[35px] rounded-full"
+            className="size-[35px] rounded-full phone:size-[40px]"
           />
           <div className="flex items-center gap-[3px]">
-            <span className="text-16p phone:text-14p font-semibold">세나</span>
+            <span className="text-16p font-semibold phone:text-14p">세나</span>
             <PiCaretRight className="text-14p phone:text-12p" />
           </div>
         </div>
