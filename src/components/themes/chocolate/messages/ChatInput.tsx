5f9bb59ae419b67a70e4fd27334b9ec13321442f
Arsture <minseok.kim@pygmalion.team>
2024-10-04 15:41:38 +0900

    feature: submit button

--- src/components/themes/chocolate/messages/ChatInput.tsx
+++ src/components/themes/chocolate/messages/ChatInput.tsx
@@ -1,5 +1,11 @@
 import { Toggle } from "konsta/react";
 import { useEffect, useRef, useState } from "react";
+import { PiArrowUpBold } from "react-icons/pi";
+import "./chat-input.css";
+
+const maxRows = 3;
+const defaultHeight = 40;
+const lineHeight = 22;
 
 export default function ChatInput() {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
@@ -8,13 +14,18 @@ export default function ChatInput() {
 
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setValue(event.target.value);
+    const maxHeight = 80;
+    if (textareaRef.current && textareaRef.current.scrollHeight <= maxHeight) {
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
   };
 
   useEffect(() => {
     if (textareaRef.current) {
       const newLineCount = value.split("\n").length;
-      if (newLineCount < 4) {
-        textareaRef.current.style.height = `${40 + (newLineCount - 1) * 22}px`;
+      if (newLineCount <= maxRows) {
+        textareaRef.current.style.height = `${defaultHeight + (newLineCount - 1) * lineHeight}px`;
       }
     }
   }, [value]);
@@ -23,7 +34,9 @@ export default function ChatInput() {
     <div
       className={`sticky bottom-0 z-50 flex h-[120px] w-full flex-row justify-stretch gap-3 bg-white px-3 pb-[30px] pt-[5px]`}
     >
-      <div className={`flex shrink flex-col items-center justify-start `}>
+      <div
+        className={`relative flex shrink flex-col items-center justify-start`}
+      >
         <div className={`text-[14px] font-normal text-[#535C62]`}>
           탈옥 토글
         </div>
@@ -37,9 +50,14 @@ export default function ChatInput() {
         value={value}
         onChange={handleChange}
         ref={textareaRef}
-        className={`h-[40px] grow resize-none rounded-2xl border border-[#F0F0F0] bg-[#F8F8F8] px-3 pt-2 text-16p font-medium text-black focus:outline-none`}
+        className={`scrollbar-hide h-[40px] grow resize-none rounded-2xl border border-[#F0F0F0] bg-[#F8F8F8] px-3 pr-10 pt-2 text-16p font-medium text-black focus:outline-none`}
         rows={1}
       />
+      {value && (
+        <PiArrowUpBold
+          className={`absolute right-4 top-2 size-9 rounded-full bg-[#FFE400] p-1.5 text-black`}
+        />
+      )}
     </div>
   );
 }
